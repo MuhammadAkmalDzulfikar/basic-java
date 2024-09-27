5aea8ec79906c343f3a6c7304b7035fea90ab1d2
zul <161696682+MuhammadAkmalDzulfikar@users.noreply.github.com>
2024-09-27 15:41:20 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+// menghitung luas persegi
+var sisi = prompt ('masukkan nilai sisi = ')
+var luas = sisi*sisi;
+console.log ("Luas persegi adalah = " + luas);
+
+
+// menghitung umur berdasarkan tahun lahir
+var tahun = prompt ('masukkan tahun Anda lahir = ')
+var umur = new Date (). getFullYear()- tahun
+console.log (` umur Anda sekarang adalah ${umur} `)
+
+//menghitung keliling lingkaran
+const jari = prompt ('masukkan jari-jari = ')
+const keliling =2*Math.phi.jari
+console.log (`keliling lingkaran adalah ${keliling}`)
+
+// menghitung luas persegi panjang
+const panjang = prompt ('masukkan panjang = ')
+const lebar = prompt ('masukkan lebar')
+const luas = panjang*lebar
+console.log (`Luas persegi panjang adalah ${luas}`)
+
+//menghitung volume kubus
+const sisi = prompt ('masukkan sisi = ')
+const volume = sisi*sisi*sisi
+console.log(`volume kubus adalah ${volume}`)
+
+//menghitung suhu
+
+const suhu = prompt ('masukkan suhu = ')
+const fahrenheit = (suhu*(9/5))+32;
+console.log(`suhu fahrenheit adalah ${fahrenheit}`)
+
+//menghitung kembalian dari total
+
+const telurperkg = 28000;
+const hargatelur = 25
+const totalbayr = jumlahtelur - 
+const kembalian = totalbayar
